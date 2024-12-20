369f5f30a969f2fb02cbff4465343d875bf86d04
Lucas Martins <127567616+LucasPMartins@users.noreply.github.com>
2024-12-20 18:52:36 -0300

    Refactor base templates; add header and head partials, implement search functionality for contacts

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,14 +1,41 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
+from django.db.models import Q
 from contact.models import Contact
 from django.http import Http404
 
 def index(request):
     contacts = Contact.objects.filter(show=True).order_by('-id')
+
     context = {
         'contacts': contacts,
+        'site_title': 'Contatos - '
     }
     return render(request, 'contact/index.html',context=context)
 
+def search(request):
+    search_value = request.GET.get('q','').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+    full_name = search_value.split(' ')
+    contacts = Contact.objects\
+        .filter(show=True)\
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(email__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(first_name__icontains=full_name[0]) |
+            Q(last_name__icontains=full_name[-1])
+        )\
+        .order_by('-id')
+    
+    context = {
+        'contacts': contacts,
+        'site_title': 'Procurar - ',
+        'search_value': search_value
+    }
+    return render(request, 'contact/index.html',context=context)
 
 def contact(request,contact_id):
     # single_contact = Contact.objects.filter(pk=contact_id).first()
@@ -18,5 +45,6 @@ def contact(request,contact_id):
 
     context = {
         'contact': single_contact,
+        'site_title': f'{single_contact.first_name} {single_contact.last_name} - '
     }
     return render(request, 'contact/contact.html',context=context)
