8ebfb082acdc6866a0b29aec41adc932ca52d3cb
Lucas Martins <127567616+LucasPMartins@users.noreply.github.com>
2024-12-23 15:38:31 -0300

    Implement contact update and delete functionality; enhance form validation and styling

--- contact/forms.py
+++ contact/forms.py
@@ -7,16 +7,18 @@ class ContactForm(forms.ModelForm):
     first_name = forms.CharField(
         widget=forms.TextInput(
             attrs={
-                'class': 'classe-a classe-b',
+                # 'class': 'classe-a classe-b',
                 'placeholder': 'Escreva seu primeiro nome',
             }
         ),
-        label='Primeiro Nome',
-        help_text='Texto de ajuda para seu usuário',
+        # label='Primeiro Nome',
+        help_text='Nome do contato',
     )
+
     def __ini__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         # self.fields['first_name'].widget.attrs.update({'placeholder': 'Escreva seu nome'})
+
     class Meta:
         model = Contact
         fields = ('first_name',
@@ -29,9 +31,24 @@ class ContactForm(forms.ModelForm):
         # widgets = {
         #     'first_name': forms.TextInput(attrs={'placeholder': 'Escreva seu nome'}),
         # }
-    def clean(self):
-        # cleaned_data = self.cleaned_data
 
-        self.add_error('first_name',ValidationError('This is a test error',code='invalid'))
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+
+        if first_name == last_name:
+            msg = ValidationError(
+                'Primeiro nome não pode ser igual ao segundo',
+                code='invalid'
+            )
+            self.add_error('first_name', msg)
+            self.add_error('last_name', msg)
 
         return super().clean()
+    
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+        if first_name == 'admin':
+            raise ValidationError('Invalid first name')
+        return first_name
